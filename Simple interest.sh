ea5d793d65bd01e5efbee0d5f45bf30cfb2d01ca
RayyanMansuri05 <163100710+RayyanMansuri05@users.noreply.github.com>
2026-02-04 14:42:37 +0530

    Update Simple interest.sh

--- Simple interest.sh
+++ Simple interest.sh
@@ -1,1 +1,15 @@
 Here it is a file of the simple interest one which is there 
+
+#!/bin/bash
+# Script to calculate simple interest
+# Formula: SI = (P * R * T) / 100
+
+echo "Enter the principal amount:"
+read p
+echo "Enter the rate of interest:"
+read r
+echo "Enter the time period (years):"
+read t
+
+s=`expr $p \* $t \* $r / 100`
+echo "The simple interest is: $s"
